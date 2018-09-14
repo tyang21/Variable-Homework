18fb7029d585cdc617f0db97684834a52a2a2f88
tyang21 <42978521+tyang21@users.noreply.github.com>
2018-09-13 21:03:08 -0400

    Add files via upload

---
+++ src/VariablesHomework.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ * @author Tyler
+ * Variables Homework
+ *Period 7
+ */
+public class VariablesHomework 
+{
+
+	public static void main(String[] args) 
+	{
+		String name = "Tyler";
+		int length = 10;
+		int width = 12;
+		int areaRectangle = length * width;
+		int radius = 3;
+		double pi = 3.14;
+		double areaCircle = pi * radius * radius;
+		double circumference = 2 * pi * radius;
+		System.out.println(name);
+		System.out.println(areaRectangle);
+		System.out.println(areaCircle);
+		System.out.println(circumference);
+		// TODO Auto-generated method stub
+
+	}
+
+}
